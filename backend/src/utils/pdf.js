1b89f8bf1126e3a5433bde20a276876d1ac0d71c
Mahmoud Ali <114455475+mahmoud9877@users.noreply.github.com>
2025-09-25 16:15:59 +0300

    Update pdf.js

--- backend/src/utils/pdf.js
+++ backend/src/utils/pdf.js
@@ -1,11 +1,13 @@
+import fs from "fs";
 import pdf from "pdf-parse";
 
 export const extractTextFromPDF = async (buffer) => {
-    try {
-        const data = await pdf(buffer);
-        return { text: data.text };
-    } catch (error) {
-        console.error("Error extracting PDF text:", error.message);
-        throw new Error("Failed to extract text from PDF.");
-    }
+  try {
+    if (!buffer) return { text: "" };
+    const data = await pdf(buffer);
+    return { text: data.text };
+  } catch (error) {
+    console.error(error.message);
+    return { text: "" };
+  }
 };
